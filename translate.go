91bfc513bfd175650ce03b49d37bc4c2ec0c57db
Locosoft <locosoft1986@gmail.com>
2015-07-26 20:45:41 +0800

    complete gotranslate main functions

--- translate.go
+++ translate.go
@@ -1,4 +1,9 @@
 package gotranslate
+import (
+	"fmt"
+	"strings"
+	"reflect"
+)
 
 type localization struct {
 	lang string
@@ -14,19 +19,71 @@ type translateStore struct {
 	currentLang 		string
 	fullbackLang 		string
 	loader 				*transLoaderBase
-	langNames 			[]string
 	locales 			map[string]*localization
 }
 
-var tranlates = &translateStore{locales: make(map[string]*localization))}
+var translates = &translateStore{locales: make(map[string]*localization)}
 
-func (self *translateStore) Add(langName string, locale localization) {
 
+func (self *translateStore) Load(langName string) {
+	if self.loader == nil {
+		fmt.Println("ERROR: Loader is not set")
+		return
+	}
+
+	langData, ok := (*self.loader).LoadLanguage(langName)
+
+	if ok {
+		locale := new(localization)
+		locale.lang = langName
+		locale.langData = langData
+
+		self.locales[langName] = locale
+	}
 }
 
-func (self *translateStore) Get(key string, loader transLoaderBase)(string, bool) {
+func (self *translateStore) Dive(keyArray []string, index int, jsonData map[string]interface{})(string, bool) {
+	var retValue string
+	var success bool
 
-	return "", false;
+	if index >= len(keyArray) {
+		return "", false
+	}
+
+	tmpVar, ok := jsonData[keyArray[index]]
+
+	if !ok {
+		return "", false
+	}
+
+	switch tmpVar.(type) {
+	case map[string]interface{}:
+		retValue, success = self.Dive(keyArray, index + 1, tmpVar.(map[string]interface{}))
+
+	case string:
+		retValue = tmpVar.(string)
+		success = true
+
+	default:
+		retValue = ""
+		success = false
+	}
+
+	return retValue, success;
+}
+
+func (self * translateStore) Get(langName string, dottedKey string) (string, bool) {
+	jsonKeys := strings.Split(dottedKey, ".")
+
+	locale, langExists := self.locales[langName]
+
+	if !langExists {
+		return "", false
+	}
+
+	retLangString, ok := self.Dive(jsonKeys, 0, locale.langData)
+
+	return retLangString, ok
 }
 
 func UseStaticFileLoader(option interface{}) {
@@ -34,29 +91,62 @@ func UseStaticFileLoader(option interface{}) {
 }
 
 func UseLoader(loader transLoaderBase, option interface{}) {
-	tranlates.loader = &loader
-	(*tranlates.loader).Config(option)
+	translates.loader = &loader
+	(*translates.loader).Config(option)
 }
 
 func Use(langName string) {
+	translates.currentLang = langName
 
+	if locale, ok := translates.locales[langName]; !ok || locale == nil {
+		translates.Load(langName)
+	}
 }
 
 func PreferredLanguage(langName string) {
-
+	translates.currentLang = langName
+	translates.Load(langName)
 }
 
 func FallbackLanguage(langName string) {
-
+	translates.fullbackLang = langName
+	translates.Load(langName)
 }
 
 func Reload(langName string) {
-
+	translates.Load(langName)
 }
 
-func TR(localeId string, params ...interface{}) string {
-	var retValue string
-	retValue = ""
+func TR(localeId string, args ...interface{}) string {
+	var retString string
+	var ok bool
 
-	return retValue
+	retString, ok = translates.Get(translates.currentLang, localeId)
+
+	if !ok {
+		retString, ok = translates.Get(translates.fullbackLang, localeId)
+	}
+
+	if !ok {
+		return ""
+	}
+
+	if len(args) > 0 {
+		params := make([]interface{}, 0, len(args))
+		for _, arg := range args {
+			if arg != nil {
+				val := reflect.ValueOf(arg)
+				if val.Kind() == reflect.Slice {
+					for i := 0; i < val.Len(); i++ {
+						params = append(params, val.Index(i).Interface())
+					}
+				} else {
+					params = append(params, arg)
+				}
+			}
+		}
+		return fmt.Sprintf(retString, params...)
+	}
+
+	return retString
 }
--- translate_test.go
+++ translate_test.go
@@ -6,7 +6,7 @@ func TestUseLoaderGivenStaticLoader(t *testing.T) {
 
 	UseLoader(staticLoader, StaticFileOption{"testprefix", "testsuffix"})
 
-	resultLoader := (*tranlates.loader).(*StaticFileLoader)
+	resultLoader := (*translates.loader).(*StaticFileLoader)
 
 	resultOpt := resultLoader.options.(StaticFileOption)
 
@@ -18,11 +18,51 @@ func TestUseLoaderGivenStaticLoader(t *testing.T) {
 func TestUseStaticFileLoaderGivenOption(t *testing.T) {
 	UseStaticFileLoader(StaticFileOption{"testPrefix", "testSuffix"})
 
-	resultLoader := (*tranlates.loader).(*StaticFileLoader)
+	resultLoader := (*translates.loader).(*StaticFileLoader)
 
 	resultOpt := resultLoader.options.(StaticFileOption)
 
 	if resultOpt.prefix != "testPrefix" || resultOpt.suffix != "testSuffix" {
 		t.Error("Expect the config option properties to be equal")
 	}
+}
+
+func TestDiveGivenKeysAndJsonMapData(t *testing.T) {
+	jsonMapData := make(map[string]interface{})
+	jsonMapData["test1"] = make(map[string]interface{})
+	(jsonMapData["test1"].(map[string]interface{}))["insideTest1"] = "This string is deep inside test1"
+	jsonMapData["test2"] = "String at test2"
+
+	keyTest1 := []string{"test1", "insideTest1"}
+	keyTest2 := []string{"test2"}
+	keyNotExistInTest1 := []string{"test1", "notExistsKey"}
+
+	resultTest1, okTest1 := translates.Dive(keyTest1, 0, jsonMapData)
+	resultTest2, okTest2 := translates.Dive(keyTest2, 0, jsonMapData)
+	resultTest3, okTest3 := translates.Dive(keyNotExistInTest1, 0, jsonMapData)
+
+	if !okTest1 {
+		t.Error("Expect okTest1 to be true")
+	}
+
+	if resultTest1 != "This string is deep inside test1" {
+		t.Error("resultTest1 is equal to 'deep inside test1' string")
+	}
+
+	if !okTest2 {
+		t.Error("Expect okTest1 to be true")
+	}
+
+	if resultTest2 != "String at test2" {
+		t.Error("resultTest1 is equal to 'String at test2' string")
+	}
+
+	if okTest3 {
+		t.Error("Expect okTest1 to be false")
+	}
+
+	if resultTest3 != "" {
+		t.Error("resultTest1 is an empty string")
+	}
+
 }
